c4a01a33b8264b76210db14b39bf79fa30d1eb3c
vain <vain.dlaigor@mail.ru>
2024-03-13 10:04:49 +0700

    Gauss

--- Algorithms.cs
+++ Algorithms.cs
@@ -34,5 +34,127 @@
             }
             return res;
         }
+
+        public static void SwapRows(Matrix matrix, int row1, int row2)
+        {
+            int cols = matrix.values.GetLength(1);
+            for (int i = 0; i < cols; i++)
+            {
+                double temp = matrix[row1, i];
+                matrix[row1, i] = matrix[row2, i];
+                matrix[row2, i] = temp;
+            }
+        }
+
+        public static double[,] AddVectorToMatrix(Matrix matrix, double[] vector)
+        {
+            int rows = matrix.values.GetLength(0);
+            int cols = matrix.values.GetLength(1);
+            double[,] result = new double[rows, cols + 1];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = matrix[i, j];
+                }
+                result[i, cols] = vector[i];
+            }
+            return result;
+        }
+
+
+        public static double[] GaussWithoutMainElement(Matrix matrix, double[] vector)
+        {
+            Matrix fullMatrix = new Matrix(AddVectorToMatrix(matrix, vector));
+            int rows = fullMatrix.values.GetLength(0);
+            int cols = fullMatrix.values.GetLength(1);
+
+            for (int i = 0; i < rows - 1; i++)
+            {
+                int firstNonZero = i;
+                while (firstNonZero < rows && fullMatrix[firstNonZero, i] == 0)
+                {
+                    firstNonZero++;
+                }
+
+                if (firstNonZero == rows)
+                {
+                    continue;
+                }
+
+                SwapRows(fullMatrix, i, firstNonZero);
+
+                for (int j = i + 1; j < rows; j++)
+                {
+                    double factor = fullMatrix[j, i] / fullMatrix[i, i];
+                    for (int k = i; k < cols; k++)
+                    {
+                        fullMatrix[j, k] -= factor * fullMatrix[i, k];
+                    }
+                }
+            }
+
+            double[] solution = new double[rows];
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < cols - 1; j++)
+                {
+                    sum += fullMatrix[i, j] * solution[j];
+                }
+                solution[i] = (fullMatrix[i, cols - 1] - sum) / fullMatrix[i, i];
+            }
+            return solution;
+        }
+
+        public static double[] GaussWithMainElement(Matrix matrix, double[] vector)
+        {
+            Matrix fullMatrix = new Matrix(AddVectorToMatrix(matrix, vector));
+            int rows = fullMatrix.values.GetLength(0);
+            int cols = fullMatrix.values.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                int maxRow = i;
+                double maxVal = Math.Abs(fullMatrix[i, i]);
+
+                for (int j = i + 1; j < rows; j++)
+                {
+                    double absVal = Math.Abs(fullMatrix[j, i]);
+                    if (absVal > maxVal)
+                    {
+                        maxVal = absVal;
+                        maxRow = j;
+                    }
+                }
+
+                if (maxRow != i)
+                {
+                    SwapRows(fullMatrix, i, maxRow);
+                }
+
+                for (int j = i + 1; j < rows; j++)
+                {
+                    double factor = fullMatrix[j, i] / fullMatrix[i, i];
+                    for (int k = i; k < cols; k++)
+                    {
+                        fullMatrix[j, k] -= factor * fullMatrix[i, k];
+                    }
+                }
+            }
+
+            double[] solution = new double[rows];
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < cols - 1; j++)
+                {
+                    sum += fullMatrix[i, j] * solution[j];
+                }
+                solution[i] = (fullMatrix[i, cols - 1] - sum) / fullMatrix[i, i];
+            }
+            return solution;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ namespace vychmat1_2
     {
         static void Main()
         {
-            MatrixConsole.EnterSystem(out Matrix matrix, out double[] bVector);
-            Console.WriteLine("\nВведено: \n");
-            MatrixConsole.PrintSystem(matrix, bVector);
-            Console.WriteLine("\nРезультат: \n");
-            foreach (var r in Algorithms.ShuttleSolve(matrix, bVector)) Console.Write(Math.Round(r, 3) + " ");
+            //MatrixConsole.EnterSystem(out Matrix matrix, out double[] bVector);
+            //Console.WriteLine("\nВведено: \n");
+            //MatrixConsole.PrintSystem(matrix, bVector);
+            //Console.WriteLine("\nРезультат: \n");
+            double[,] matm = {
+            {2.83333f, 5, 1},
+            {1.7f, 3, 7},
+            {8, 1, 1}
+            };
+            double[] vect = { 11.66666f, 13.4f, 18 };
+            Matrix matrix = new Matrix(matm); 
+            foreach (var r in Algorithms.GaussWithMainElement(matrix,vect)) Console.Write(Math.Round(r, 3) + " ");
         }
     }
 }
